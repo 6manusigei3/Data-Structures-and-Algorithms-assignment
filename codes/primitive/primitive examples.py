756873d2b681c779e33e8466e9f24b3dcae39fad
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-02-16 00:32:49 +0300

    Update primitive examples.py

--- codes/primitive/primitive examples.py
+++ codes/primitive/primitive examples.py
@@ -1,1 +1,12 @@
 
+
+integer_value = 10
+float_value = 3.14
+char_value = 'A'
+boolean_value = True
+
+print("Integer:", integer_value)
+print("Float:", float_value)
+print("Character:", char_value)
+print("Boolean:", boolean_value)
+
