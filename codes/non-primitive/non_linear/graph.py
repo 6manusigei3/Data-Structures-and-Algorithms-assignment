5f6914d14076bedd21353875bca9e6425f496d7b
Manu_Sigei <kipchumbaemmanuel061@gmail.com>
2026-02-16 00:45:05 +0300

    Update graph.py

--- codes/non-primitive/non_linear/graph.py
+++ codes/non-primitive/non_linear/graph.py
@@ -1,1 +1,13 @@
 
+
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "D"],
+    "C": ["A"],
+    "D": ["B"]
+}
+
+print("Graph representation:")
+for node in graph:
+    print(node, "->", graph[node])
+
